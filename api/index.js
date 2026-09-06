764b657f8ccec33edc7790ba5f03b8f7324ab858
jigneshbhandi09 <jignesh@finela.ai>
2026-09-06 13:48:37 +0530

    Expose exact serverless initialization error in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
-const app = require('../server/src/app');
-
-module.exports = app;
-
+module.exports = async (req, res) => {
+  try {
+    const app = require('../server/src/app');
+    return app(req, res);
+  } catch (err) {
+    console.error('SERVERLESS INIT ERROR:', err);
+    return res.status(500).json({
+      error: 'SERVERLESS_INIT_ERROR',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
