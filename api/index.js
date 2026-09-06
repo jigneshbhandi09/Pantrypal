fe6833e66f030c3e865bd270b8da26fd4c486615
jigneshbhandi09 <jignesh@finela.ai>
2026-09-06 14:01:33 +0530

    Fix jwt.sign parameter signature in authController.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 const app = require('../server/src/app');
 
 module.exports = app;
+
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -15,3 +15,4 @@ const userSchema = new Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
+
